dda670a122b484ba06d8e8236a89b8beebde97e2
qin2979517008 <2979517008@qq.com>
2019-11-06 13:22:15 +0800

    second commit

---
+++ four-lv-1.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("input")
+	var str [5] int64
+	for i:=0;i<len(str);i++{
+		fmt.Scanf("%d\n",&str[i])
+	}
+
+	for a:=0;a<len(str);a++{
+		if str[a] !=0{
+			fmt.Println("input ok!")
+		}
+	}
+
+fmt.Println("the result are :")
+	for j:=0;j<len(str);j++{
+		b:=str[j]
+		a:=time.Unix(b,1)
+		fmt.Println(a)
+	}
+
+}
